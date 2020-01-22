e22d4e3437f6244330825be3dba56c0aaef304aa
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-01-22 15:17:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,5 +19,10 @@ class App < Sinatra::Base
     @user_name = params[:name]
     "Goodbye, #{@user_name}."
   end
+  
+  get "/multiply/:num1/:num2" do
+    @sum = params[:num1].to_i * params[:num2].to_i
+    @sum.to_s
+  end
 
 end
